a1c3275f386a3726e91e206c3163f279b411ad17
krishnendu <krishnenduduttadc@gmail.com>
2023-06-04 20:14:59 +0530

    second commit

--- src/Arrays/DiagonalTraversal.java
+++ src/Arrays/DiagonalTraversal.java
@@ -19,6 +19,13 @@ public class DiagonalTraversal {
             {25, 26, 27, 28, 29, 30},
             {31, 32, 33, 34, 35, 36}
         };
+        
+        for (int g = 0; g < arr.length; g++) {
+            for (int i = 0,j=g; j<arr.length; i++,j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println("");
+        }
     }
     
 }
